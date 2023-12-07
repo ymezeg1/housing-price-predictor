35ee54ff8549867fe1ad049358bdbea34e5e9398
ymezeg1 <153127168+ymezeg1@users.noreply.github.com>
2023-12-07 16:11:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def estimate(rm,lstat,ptratio,confidence):
   stats[0][ptratioi]=ptratio
 
   logPredict=r.predict(stats)[0][0]
-  if confidence==True:
+  if confidence.any():
     upper=logPredict+2*root
     lower=logPredict-2*root
     interval=95
