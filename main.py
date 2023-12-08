181294a1302cd00ad60090e1441c280a16f84be0
ymezeg1 <153127168+ymezeg1@users.noreply.github.com>
2023-12-08 13:41:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ plt.figure(figsize=(8,16))
 
 for i,num in enumerate(housingData.select_dtypes(exclude='O')):
     ax=plt.subplot(6,2,i + 1)
-    sns.histplot(x=housingData[num],ec='black',color='#7A33FF')
+    sns.histplot(x=housingData[num],ec='black',color='#626262')
     plt.title(num, fontsize=10)
     
 plt.tight_layout()
